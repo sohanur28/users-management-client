77ace67d499873de63c896e4b9454670967eba05
sohanur28 <gsrahman28@gmail.com>
2023-05-14 16:12:33 +0600

    data post

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,48 @@ function App() {
 
   const [users, setUsers] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch('http://localhost:5000/users')
-    .then(res => res.json())
-    .then(data => setUsers(data))
+      .then(res => res.json())
+      .then(data => setUsers(data))
   }, [])
 
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email}
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('inside post response', data)
+    })
+  }
+
   return (
-    <>      
+    <>
       <h1>Users Management System</h1>
-       <h3>Numbers of Users: {users.length}</h3>
+      <h3>Numbers of Users: {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <div>
+        {
+          users.map(user => <p key={user.id}> {user.id} : {user.name} : {user.email}</p>)
+        }
+      </div>
     </>
   )
 }
